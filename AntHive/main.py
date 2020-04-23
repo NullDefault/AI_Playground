8d08f122b4c74bd5dca685ec0635334fb35d3fba
David Nesterov-Rappoport <davisha999@gmail.com>
2020-04-23 17:04:31 -0400

    more work on ants

--- AntHive/main.py
+++ AntHive/main.py
@@ -1,60 +1,16 @@
 import pygame
-
-from AntHive.ant import Ant
+from os.path import join
+from AntHive.logic_functions import process_key, process_mouse
 from AntHive.map import Map
+from random import randint
 
 map_dims = (1000, 500)
-frame_size = (1800, 1000)
-s = 10
+frame_size = (1600, 900)
+s = 7
 game_map = Map(map_dims[0], map_dims[1], s)
 surf = pygame.display.set_mode(frame_size)
-ants = pygame.sprite.RenderUpdates()
-
-def check_camera_edges(x_off, y_off):
-    if y_off < 0:
-        y_off = 0
-    if y_off > map_dims[1] * 4 - frame_size[1]:
-        y_off = map_dims[1] * 4 - frame_size[1]
-    if x_off > map_dims[0] * 4 - frame_size[0]:
-        x_off = map_dims[0] * 4 - frame_size[0]
-    if x_off < 0:
-        x_off = 0
-
-    return x_off, y_off
-
-
-def process_key(k, x_off, y_off):
-    if k == 'w' or k == 'up':
-        y_off = y_off - 100
-    elif k == 's' or k == 'down':
-        y_off = y_off + 100
-    elif k == 'd' or k == 'right':
-        x_off = x_off + 100
-    elif k == 'a' or k == 'left':
-        x_off = x_off - 100
-
-    return check_camera_edges(x_off, y_off)
-
-
-def process_mouse(x_off, y_off, drag_a):
-    if event.type == pygame.MOUSEBUTTONUP:
-        if event.button == 1:
-            drag_b = pygame.Vector2(event.pos)
-            drag_direction = drag_a - drag_b
-            x_off = int(x_off + drag_direction[0])
-            y_off = int(y_off + drag_direction[1])
-
-    elif event.type == pygame.MOUSEBUTTONDOWN:
-        print(event.button)
-        if event.button == 1:
-            mouse_x, mouse_y = event.pos
-            drag_a = pygame.Vector2(mouse_x, mouse_y)
-        elif event.button == 3:
-            ants.add(Ant(event.pos))
-
-    offs = check_camera_edges(x_off, y_off)
-    return drag_a, offs[0], offs[1]
-
+ant_sprite = pygame.image.load(join("sprites", "ant.png"))
+ants = []
 
 x_offset = 0
 y_offset = 0
@@ -64,17 +20,23 @@ drag_begin = None
 running = True
 while running:
     map_surf = game_map.render()
-    map_surf = pygame.transform.scale(map_surf, (map_dims[0]*4, map_dims[1]*4))
+    map_surf = pygame.transform.scale(map_surf, (map_dims[0]*s, map_dims[1]*s))
     surf.blit(map_surf, dest=(0, 0), area=pygame.Rect(0+x_offset, 0+y_offset, frame_size[0], frame_size[1]))
-    ants.draw(surf)
+    for ant in ants:
+        ant.update(surf, x_offset, y_offset)
+        if (x_offset <= ant.loc.x <= x_offset+frame_size[0]) and (y_offset <= ant.loc.y <= y_offset+frame_size[1]):
+            temp = ant_sprite.copy()
+            temp = pygame.transform.rotate(temp, ant.angle)
+            surf.blit(temp, (int(ant.loc.x-x_offset), int(ant.loc.y-y_offset)))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             key = pygame.key.name(event.key)
-            x_offset, y_offset = process_key(key, x_offset, y_offset)
+            x_offset, y_offset = process_key(key, x_offset, y_offset, s)
         else:
-            drag_begin, x_offset, y_offset = process_mouse(x_offset, y_offset, drag_begin)
+            drag_begin, x_offset, y_offset, ants = process_mouse(
+                x_offset, y_offset, drag_begin, event, ants, map_dims, frame_size, s)
     pygame.display.update()
 pygame.quit()
 
